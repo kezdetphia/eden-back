04ec6f11b9757ba78cdbb61d772296cdeedc1e2e
kezdetphia <fehermark88@gmail.com>
2024-10-01 14:49:41 -0700

    changed route for testing

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -4,7 +4,9 @@ const ProductController = require("../controllers/ProductController");
 const { verifyToken } = require("../middleware/veryfiToken");
 
 // Register a new user
-router.get("/getproducts", verifyToken, ProductController.getAllProducts);
+// router.get("/getproducts", ProductController.getAllProducts);
+router.get("/getproducts", ProductController.getAllProducts);
+// router.get("/getproducts", verifyToken, ProductController.getAllProducts);
 
 router.post("/createproduct", verifyToken, ProductController.createProduct);
 
